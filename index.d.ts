8ebb0e95bc94c2a324187d7bb67028fc7a8c8d48
sirajalam049 <sirajalam049@gmail.com>
2019-08-01 14:26:22 +0530

    Some typos and types fixed

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
-declare model "use-reducer-logger" {
-  type Reducer<State = any, Action = any> = (state: State, action: Action) => State; 
-  export logger<State, Action> = (reducer: Reducer) => Reducer;
+declare module "use-reducer-logger" {
+    type Reducer<State = any, Action = any> = (state: State, action: Action) => State;
+    const logger = (reducer: Reducer<State, Action>):Reducer => Reducer;
+    export default logger
 }
