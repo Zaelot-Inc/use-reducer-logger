272100f59d9fa24f5dc2fbb64a11a41287f3db12
Thordur Thordarson <thor@eml.cc>
2020-11-09 21:13:43 -0500

    Missing from the cherry-pick

--- srhfl.js
+++ srhfl.js
@@ -16,6 +16,7 @@ const logger = (reducer) => {
     console.log("%cPrevious State:", "color: #9E9E9E; font-weight: 700;", state);
     console.log("%cAction:", "color: #00A7F7; font-weight: 700;", action);
     console.log("%cNext State:", "color: #47B04B; font-weight: 700;", next);
+    console.groupEnd();
     return next;
   }, [reducer]);
 
